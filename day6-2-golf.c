afc26a7c4d23bda9a123f6c0749083157cbf1f84
Eisenwave <me@eisenwave.net>
2021-12-06 14:27:13 +0100

    Update day6-2-golf.c

--- day6-2-golf.c
+++ day6-2-golf.c
@@ -1,1 +1,1 @@
-long f[9],i,k,s;main(){while(scanf("%lu,",&i)>0)++f[i],++s;for(;k<256;++k)i=f[k%9],f[(k+7)%9]+=i,s+=i;printf("%lu",s);}
+long f[9],i,k,s;main(){while(scanf("%ld,",&i)>0)++f[i],++s;for(;k<256;++k)i=f[k%9],f[(k+7)%9]+=i,s+=i;printf("%ld",s);}
