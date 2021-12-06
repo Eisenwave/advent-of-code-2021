8b8fb59c9e377f9ee75e05fb9751ad4ba5d1358b
Eisenwave <me@eisenwave.net>
2021-12-06 14:03:26 +0100

    Update day6-2-golf.c

--- day6-2-golf.c
+++ day6-2-golf.c
@@ -1,1 +1,1 @@
-j;main(){typeof(0ul)f[9]={},i;while(scanf("%lu,",&i)>0)++f[i];for(i=0;i<256;++i)f[(i+7)%9]+=f[i%9];for(i=0;j<9;++j)i+=f[j];printf("%lu",i);}
+j;main(){typeof(0ul)f[9]={},i;while(scanf("%lu,",&i)>0)++f[i];for(i=0;i<256;++i)f[(i+7)%9]+=f[i%9];for(i=0;j++<9;)i+=f[j-1];printf("%lu",i);}
